cca5148f56e4f677be132c4f1cdd8a1b13a2270c
dsundarpaul <109967226+dsundarpaul@users.noreply.github.com>
2025-01-25 17:47:20 +0530

    inital try to self setup

---
+++ packages/server/dist/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { WebSocketServer } from 'ws';
+const app = express();
+const PORT = 5000;
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT} 🚀`);
+});
+const wss = new WebSocketServer({ server });
+wss.on('connection', (ws) => {
+    console.log('A new client connected');
+    ws.on('message', (message) => {
+        console.log(`Received: ${message}`);
+        ws.send(`Echo: ${message}`);
+    });
+});
